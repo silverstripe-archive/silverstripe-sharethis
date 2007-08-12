688f2f275e1b97c55e1b8e5df377e723d3fd0827
Lakshan Perera <lakshan@web2media.net>
2007-08-12 01:15:39 +0000

    Ooops...committed wrong files FIXED.

--- _config.php
+++ _config.php
@@ -1,5 +1,31 @@
 <?php
+  
+/**
+ *add decorator to SiteTree
+*/
+DataObject::add_extension('SiteTree', 'ShareIcons');
+SiteTree::ExtendCMS(array('ShareIcons', 'updateCMSFields'));
 
-//Visit http://www.flickr.com/services/api/keys/apply/ to get your own API key in one quick easy step. 
-FlickrService::setAPIKey('c88164229499662e1497335950c61eeb'); 
+/**
+ * Set Stylesheets
+*/
+if(Director::fileExists(project() . "/css/ShareThis.css"))
+         Requirements::css(project() . "/css/ShareThis.css");
+      else
+         Requirements::css("sharethis/css/ShareThis.css");
+
+/**
+ *Custom Properties
+*/
+
+ShareIcons::$IconTransparent = true; //sets the transparency of the icons
+ShareIcons::$ShowTitle = false; //shows title of the icon in text, next to the icon
+
+/**
+ *Enabled icons
+ *We recommend not to have more than 5 icons, to avoid the visual noise.
+*/
+ShareIcons::$EnabledIcons = array('digg', 'reddit', 'delicious', 'simpy', 'google', 'ma.gnolia');
+
+
 ?>
