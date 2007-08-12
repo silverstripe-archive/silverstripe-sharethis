24deecee3449386077d58dbceab02219fd602274
Lakshan Perera <lakshan@web2media.net>
2007-08-12 01:10:55 +0000

    Minor changes in comments

--- _config.php
+++ _config.php
@@ -1,31 +1,5 @@
 <?php
-  
-/**
- *add decorator to SiteTree
-*/
-DataObject::add_extension('SiteTree', 'ShareIcons');
-SiteTree::ExtendCMS(array('ShareIcons', 'updateCMSFields'));
 
-/**
- * Set Stylesheets
-*/
-if(Director::fileExists(project() . "/css/ShareThis.css"))
-         Requirements::css(project() . "/css/ShareThis.css");
-      else
-         Requirements::css("sharethis/css/ShareThis.css");
-
-/**
- *Custom Properties
-*/
-
-ShareIcons::$IconTransparent = true; //remove this if you want to make
-ShareIcons::$ShowTitle = false; //shows title of the icon in text next to the icon
-
-/**
- *Enabled icons
- *We recommend not to have more than 5 icons, to avoid the visual noise.
-*/
-ShareIcons::$EnabledIcons = array('digg', 'reddit', 'delicious', 'simpy', 'google', 'ma.gnolia');
-
-
+//Visit http://www.flickr.com/services/api/keys/apply/ to get your own API key in one quick easy step. 
+FlickrService::setAPIKey('c88164229499662e1497335950c61eeb'); 
 ?>
